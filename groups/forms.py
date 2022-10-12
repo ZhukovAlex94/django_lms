d769291b07f078f9a238f951ad7b26a6a9274e31
Zhukov Oleksandr <zhukov_alex94@icloud.com>
2022-10-12 11:13:47 +0300

    django lesson 7

--- groups/forms.py
+++ groups/forms.py
@@ -5,22 +5,7 @@ from django_filters import FilterSet
 from .models import Group
 
 
-# class GroupBaseForm(forms.ModelForm):
-#     class Meta:
-#         model = Group
-#         fields = [
-#             'name',
-#             'group_start_date',
-#             'group_end_date',
-#         ]
-#
-#         widgets = {
-#             'group_start_date': forms.DateInput(attrs={'type': 'date'}),
-#             'group_end_date': forms.DateInput(attrs={'type': 'date'}),
-#         }
-
-
-class CreateGroupForm(forms.ModelForm):
+class GroupBaseForm(forms.ModelForm):
     class Meta:
         model = Group
         fields = [
@@ -36,24 +21,15 @@ class CreateGroupForm(forms.ModelForm):
         }
 
 
-class UpdateGroupForm(forms.ModelForm):
-    class Meta:
-        model = Group
-        fields = [
-            # '__all__',
-            'group_name',
-            'group_start_date',
-            'group_end_date',
-            'group_description',
-        ]
+class CreateGroupForm(GroupBaseForm):
+    pass
 
-        widgets = {
-            'group_start_date': forms.DateInput(attrs={'type': 'date'}),
-            'group_end_date': forms.DateInput(attrs={'type': 'date'}),
-        }
-        # exclude = [
-        #     'group_start_date',
-        # ]
+
+class UpdateGroupForm(GroupBaseForm):
+    class Meta(GroupBaseForm.Meta):
+        exclude = [
+            'group_start_date',
+        ]
 
 
 class GroupFilterForm(FilterSet):
