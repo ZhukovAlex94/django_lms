b6816ac7a76d9994f046d1940934d401738627b2
Zhukov Oleksandr <zhukov_alex94@icloud.com>
2022-10-25 18:30:04 +0300

    django hw 11

--- groups/admin.py
+++ groups/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from groups.models import Group
+
+from rangefilter.filters import DateRangeFilter
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('group_name', 'group_description', 'group_start_date', )
+    list_display_links = list_display
+    list_filter = (('group_start_date', DateRangeFilter), 'group_name',)
+    fieldsets = (
+        ('Group info', {'fields': ('group_name',)}),
+        ('Group dates', {'fields': ('group_start_date', 'group_end_date')}),
+        ('Group description', {'fields': ('group_description',)}),
+        ('Teachers and Headman', {'fields': ('teachers', 'headman')}),
+    )
+
+
+admin.site.register(Group, GroupAdmin)
--- teachers/admin.py
+++ teachers/admin.py
@@ -1,3 +1,79 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
 
-# Register your models here.
+from teachers.models import Teacher
+
+from datetime import date
+
+
+class SalaryListFilter(admin.SimpleListFilter):
+    title = 'salary filter'
+    parameter_name = 'salary_filter'
+
+    def lookups(self, request, model_admin):
+
+        return [
+            ('< 10000', '< less than 10000'),
+        ] + [
+            (f'{i}0000 - {i+1}0000', f'{i}0000 - {i+1}0000') for i in range(1, 10)
+        ] + [
+            ('> 100000', '> more than 100000')
+        ]
+
+    def queryset(self, request, queryset):
+        value = self.value().split() if self.value() is not None else None
+        match value:
+            case ['<', amount]:
+                return Teacher.objects.filter(salary__lt=amount)
+            case ['>', amount]:
+                return Teacher.objects.filter(salary__gte=amount)
+            case [gte, '-', lt]:
+                return Teacher.objects.filter(salary__gte=gte, salary__lt=lt)
+            case _:
+                return Teacher.objects.all()
+
+
+class AgeListFilter(admin.SimpleListFilter):
+    title = 'age filter'
+    parameter_name = 'age_filter'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('< 18', '< less than 18 years old'),
+        ] + [
+            ('> 18', '> more than 18 years old'),
+        ]
+
+    def queryset(self, request, queryset):
+        today = date.today()
+        value = self.value().split() if self.value() is not None else None
+        match value:
+            case ['<' | '>' as op, age] if age.isdigit():
+                min_date_required = date(today.year - int(age), today.month, today.day)
+                if op == '<':
+                    return Teacher.objects.filter(birthday__lt=min_date_required)
+                else:
+                    return Teacher.objects.filter(birthday__gte=min_date_required)
+            case _:
+                return Teacher.objects.all()
+
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'salary')
+    list_display_links = list_display
+    list_per_page = 15
+    list_filter = (SalaryListFilter, AgeListFilter)
+    fieldsets = (
+        ('Personal information', {'fields': ('last_name', 'first_name')}),
+        ('DOB', {'fields': ('birthday', 'age')}),
+        ('Contacts', {'fields': ('email', 'phone_number')}),
+        ('Salary', {'fields': ('salary',)})
+    )
+
+    def age(self, instance):
+        return f'{instance.get_age()} y.o.'
+
+    readonly_fields = ('age',)
+    age.short_description = 'age'
+
+
+admin.site.register(Teacher, TeacherAdmin)
