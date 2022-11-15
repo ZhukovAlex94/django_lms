6915ad55eff8b150bf62e33659cb4f6384f07f5d
Zhukov Oleksandr <zhukov_alex94@icloud.com>
2022-11-15 20:38:54 +0200

    django lesson 14

--- groups/admin.py
+++ groups/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.urls import reverse
+from django.utils.html import format_html
+from django.utils.http import urlencode
 
 from groups.models import Group
 
@@ -12,6 +15,13 @@ class StudentInlineTable(admin.TabularInline):
     extra = 0
     readonly_fields = fields
     # show_change_link = True
+
+    def get_queryset(self, request):
+        queryset = self.model.objects.filter(
+            group_id=int(request.resolver_match.kwargs['object_id'])
+        ).select_related('group')
+
+        return queryset
 
     def has_delete_permission(self, request, obj=None):
         return False
@@ -47,7 +57,18 @@ class TeacherInlineTable(admin.TabularInline):
 
 @admin.register(Group)
 class GroupAdmin(admin.ModelAdmin):
-    list_display = ('group_name', 'group_start_date', 'group_start_date',)
+    list_display = ('group_name', 'group_start_date', 'group_start_date', 'count_of_students_link')
+
+    def count_of_students_link(self, obj):
+        count = obj.students.count()
+        url = (
+            reverse('admin:students_student_changelist') + '?' + urlencode({'group_id': f'{obj.pk}'})
+        )
+
+        return format_html('<a href="{}">{} student(s)</a>', url, count)
+
+    count_of_students_link.short_description = 'Students'
+
     list_display_links = list_display
     list_filter = (('group_start_date', DateRangeFilter), 'group_name',)
     fieldsets = (
@@ -66,6 +87,7 @@ class GroupAdmin(admin.ModelAdmin):
         form.base_fields['headman'].widget.can_change_related = False
         form.base_fields['headman'].widget.can_delete_related = False
         form.base_fields['headman'].widget.can_view_related = False
+        form.base_fields['headman'].queryset = obj.students.all()
 
         return form
 
